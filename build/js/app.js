cca13eb73c767b2fa1f90760efdf2e4e4616a864
potapenkom <potapenko05121985@gmail.com>
2018-10-20 23:29:41 +0300

    nav

--- build/js/app.js
+++ build/js/app.js
@@ -1,8 +1,32 @@
-function toggleButton() {
-    var x = document.getElementById("menu");
-    if (x.className === "header__navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "header__navbar";
+$(function(){
+	$('#toggle').on('click', function() {
+		$('#nav').slideToggle(300);
+		if ($(this).css('display') === 'none') {
+			$(this).removeAttr('style');
+		}
+	});
+	  });
+
+$('#corousel').owlCarousel({
+    center: true,
+	items: 4,
+	nav:false,
+	dots: false,
+    loop:true,
+	margin: 0,
+	stagePadding: 0,
+	responsiveClass:true,
+    responsive:{
+		0:{
+			items:2
+        },
+        600:{
+			items:3,
+			center: true
+        },
+        1001:{
+			items:4,
+			center: true
+        }
     }
-}
+});
